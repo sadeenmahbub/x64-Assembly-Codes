ce743831ea6ca106e08945c102d82140140d07ef
Mobin <35500842+sadeenmahbub@users.noreply.github.com>
2018-01-20 17:07:52 +0600

    Add files via upload

---
+++ test.asm
@@ -0,0 +1,26 @@
+segment .data
+;switch: dq main.case0
+;	dq main.case1
+;	dq main.case2
+i:	dq 100
+fmt: db "%d",10,0
+segment .text
+global main
+extern printf
+main:
+
+	push rbp
+
+	mov rdi, fmt
+	mov rsi, [i]
+	call printf
+
+	mov rdi, fmt
+	mov rsi, 50
+	call printf
+
+	mov rax, 0
+	pop rbp
+	ret
+
+
