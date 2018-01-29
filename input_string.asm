f7e72961c927996dd7a13d8cdd3daa670f70e874
Mobin <35500842+sadeenmahbub@users.noreply.github.com>
2018-01-29 09:08:58 +0000

    Update input_string.asm

--- input_string.asm
+++ input_string.asm
@@ -1,54 +1,56 @@
 segment .data
-text1 db "What is your name?",10
-text2 db "Hello, "
+	text1 db "What is your name?",10
+	text2 db "Hello, "
 
 segment .bss
-name resb 16
+	name resb 16
 
 segment .text
-global main
-extern printf
+	global main
+	extern printf
 main:
 
-call _printtext1
-call _getname
-call _printtext2
-call _printname
+	call _printtext1
+	call _getname
+	call _printtext2
+	call _printname
 
-mov rax, 60
-mov rdi,0
-syscall
+	mov rax, 60
+	mov rdi,0
+	syscall
 
 _getname:
-mov rax, 0
-mov rdi, 0
-mov rsi, name
-mov rdx, 16
-syscall
-ret
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, name
+	mov rdx, 16
+	syscall
+	ret
 
 _printtext1:
-mov rax, 1
-mov rdi, 1
-mov rsi, text1
-mov rdx, 19
-syscall
-ret
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text1
+	mov rdx, 19
+	syscall
+	ret
 
 _printtext2:
-mov rax, 1
-mov rdi, 1
-mov rsi, text2
-mov rdx, 7
-syscall
-ret
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text2
+	mov rdx, 7
+	syscall
+	ret
 
 _printname:
-mov rax, 1
-mov rdi, 1
-mov rsi, name
-mov rdx, 16
-syscall
-ret
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, name
+	mov rdx, 16
+	syscall
+	ret
+
+
 
 
