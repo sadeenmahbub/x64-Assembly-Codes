24446b5b47015dc73d9ea532c6a1844b02cab86b
Mobin <35500842+sadeenmahbub@users.noreply.github.com>
2018-01-30 11:03:36 +0600

    Update input&mul_div.asm

--- input&mul_div.asm
+++ input&mul_div.asm
@@ -1,9 +1,8 @@
 segment .data
-fmt: db "Result is: %d * %d = %d",10,0
-fmt2: db "Result is: %d / %d = %d, Remainder: %d",10,0
+fmt: db "Multiplication: %d * %d = %d",10,0
+fmt2: db "Division: %d / %d = %d",10, "Remainder: %d",10,0
 
-fmt3: db "%d",10,0
-fmt4: db "%d",0
+fmt3: db "%d",0
 
 value1 dd 11
 value2 dd -2
@@ -14,9 +13,11 @@ val1 resb 16
 val2 resb 16
 
 segment .text
+
 global main
 extern printf
 extern scanf
+
 main:
 
 	push rbp
@@ -27,7 +28,7 @@ main:
 	call scanf
 	
 	
-	mov rdi, fmt4
+	mov rdi, fmt3
 	mov rsi, val2
 	mov al, 0
 	call scanf
@@ -35,20 +36,20 @@ main:
 ;For Multiplication
 
 	mov rdi, fmt		
-	mov rsi, [val1]	;1st output
+	mov rsi, [val1]		;1st output
 	mov rax, [val1]
 	mov rcx, [val2]
 
 	imul rcx
 	mov rcx, rax 		;3rd output
-	mov rdx, [val2]	;2nd output
+	mov rdx, [val2]		;2nd output
 	call printf
 	
 
 ;For Division
 	
 	mov rdi, fmt2
-	mov rsi, [val1]	;1st output
+	mov rsi, [val1]		;1st output
 	mov rax,  [val1]
 	mov rcx, [val2]
 	mov rdx, 0
@@ -56,17 +57,11 @@ main:
 	idiv dword [val2]
 	mov r8, rdx		;4th output
 	mov rcx, rax		;3rd output
-	mov rdx, [val2]	;2nd output
+	mov rdx, [val2]		;2nd output
 	call printf
 
 	mov rax, 0
 	pop rbp
-	ret
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, res
-	mov rdx, 16
-	syscall
 	ret
 
 
